3e8a5d7302b6f82ce8391851d10e850a9237e2e9
ValmirJrDS <valmirmoreirajunior@gmail>
2025-04-01 18:23:53 -0300

    Resolução desafio aula 02

--- Python_para_dados/aula02/projeto/desafio.py
+++ Python_para_dados/aula02/projeto/desafio.py
@@ -35,13 +35,13 @@ except ValueError:
 
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
-# bonus_usuario = float(input("Digite o seu bonus: "))
+bonus_usuario = float(input("Digite o seu bonus: "))
 
 # 4) Calcule o valor do bônus final
 
-# valor_do_bonus = CONSTANTE_BONUS + salario_usuario * bonus_usuario
+valor_do_bonus = CONSTANTE_BONUS + salario_usuario * bonus_usuario
 
 # 5) Imprime a mensagem personalizada incluindo o nome do usuário e o valor do bonus
-# print(f"O usuario {nome_usuario} possui o bonus de {valor_do_bonus}")
+print(f"O usuario {nome_usuario} possui o bonus de {valor_do_bonus}")
 
 # Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
